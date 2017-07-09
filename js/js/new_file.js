d6762bf909815ba86d3c6ac13fa2a24523a604dc
zhanlili <946192344@qq.com>
2017-07-09 20:12:54 +0800

    我的简历

---
+++ js/js/new_file.js
@@ -0,0 +1,26 @@
+
+			$(document).ready(function() {
+				$.fn.fullpage({
+					slidesColor: ['#f5b8b0', '#f5b8b0', '#7BAABE', '#f90', '#fabdc6'],
+					anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
+					menu: '#menu',
+					navigation: true,
+					loopBottom: true,
+					afterLoad: function(anchorLink, index) {
+						if(index == 2) {
+							$('.section2').find('.mas').delay(600).addClass("bounceInLeft")
+						}
+						if(index == 3) {
+							$('.section3').find('h3').delay(600).addClass("bounceInRight")
+						}
+						if(index == 4) {
+							$('.section4').find('.project').delay(600).addClass("zoomInRight")
+						}
+						if(index == 5) {
+							$('.section5').find('.wu1').delay(800).addClass("zoomInDown")
+						}
+					}
+				});
+				$("div[data-anchor]").height(974)
+			});
+	
